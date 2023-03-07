772ea8a0871f0570aafdf596544c04723384d018
Miguel Angel Villegas <chumilo227@gmail.com>
2023-03-07 12:00:11 -0500

    adding a context for login and logout

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import { AuthProvider } from './auth';
 import Navbar from './ui/components/Navbar';
 
 const HeroesApp = () => {
 	return (
-		<>
+		<AuthProvider>
 			<Navbar />
 			<Outlet />
-		</>
+		</AuthProvider>
 	);
 };
 
